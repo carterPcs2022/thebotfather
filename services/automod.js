5b0e15e2660be3d3ffba8c41c61e319daa9dd496
ky <circusbaby208@gmail.com>
2026-09-20 18:23:16 -0400

    Correct AutoMod blocked-word filter

--- services/automod.js
+++ services/automod.js
@@ -109,7 +109,7 @@ async function handleMessage(message) {
     }
   }
 
-  if (settings.automod_bad_words_enabled && BAD_WORDS.some(word => new RegExp('(^|\\s)' + word.replace(/[.*+?^${}()|[\\]\\\\]/g, '\\\\  if (settings.automod_mention_enabled') + '(?=$|\\s)', 'i').test(content))) {
+  if (settings.automod_bad_words_enabled && BAD_WORDS.some(word => new RegExp('(^|\\s)' + word.replace(/[.*+?^${}()|[\\]\\\\]/g, '\\\\$&') + '(?=$|\\s)', 'i').test(content))) {
     return applyAction(message, 'blocked word filter', settings);
   }
 
